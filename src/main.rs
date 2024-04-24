84c7a34f84ed1397576cd70dd7ace23a0cf70569
Jack Arthur Harrhy <me@jackharrhy.com>
2024-04-24 13:03:21 -0230

    everything aligned nice and good now... :)

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ impl GameState {
         let mut grid = vec![0; (GRID_SIZE * GRID_SIZE) as usize];
 
         grid[0] = 1;
-        grid[128] = 1;
 
         Self {
             grid,
@@ -33,7 +32,7 @@ impl GameState {
     }
 }
 
-static CELL_SIZE: f32 = 0.9;
+static CELL_SIZE: f32 = 1.0;
 
 fn setup(_state: &mut GameState, _c: &mut EngineContext) {
     let mut camera = main_camera_mut();
@@ -44,11 +43,11 @@ fn setup(_state: &mut GameState, _c: &mut EngineContext) {
 }
 
 fn update(state: &mut GameState, _c: &mut EngineContext) {
-    // draw_rect(splat(GRID_SIZE / 2.0), splat(GRID_SIZE), ORANGE, 0);
+    draw_rect(splat(GRID_SIZE / 2.0), splat(GRID_SIZE), ORANGE, 0);
 
     for (index, c) in state.grid.iter().enumerate() {
-        let y = ((index as f32) / GRID_SIZE).floor() + CELL_SIZE / 1.5;
-        let x = ((index as f32) % GRID_SIZE) + CELL_SIZE / 1.5;
+        let y = ((index as f32) / GRID_SIZE).floor() + CELL_SIZE / 2.0;
+        let x = ((index as f32) % GRID_SIZE) + CELL_SIZE / 2.0;
         let color = if *c == 1 { GREEN } else { RED };
 
         draw_rect(vec2(x, y), splat(CELL_SIZE), color, 0);
